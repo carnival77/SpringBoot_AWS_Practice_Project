e2fa15b0965286f35f5e80aaa5c67ddb118b70a5
carnival77 <stam0325@hanmail.net>
2021-05-17 18:01:12 +0900

    implement google login function

--- src/main/java/com/dms/web/IndexController.java
+++ src/main/java/com/dms/web/IndexController.java
@@ -1,9 +1,6 @@
 package com.dms.web;
 
-//import com.jojoldu.book.springboot.config.auth.LoginUser;
-//import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
-//import com.jojoldu.book.springboot.service.PostsService;
-//import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.dms.config.auth.dto.SessionUser;
 import com.dms.service.PostsService;
 import com.dms.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/save")
     public String postsSave() {
@@ -26,6 +26,11 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
 
         return "index";
     }
@@ -38,17 +43,4 @@ public class IndexController {
         return "posts-update";
     }
 
-//
-//    @GetMapping("/posts/save")
-//    public String postsSave() {
-//        return "posts-save";
-//    }
-//
-//    @GetMapping("/posts/update/{id}")
-//    public String postsUpdate(@PathVariable Long id, Model model) {
-//        PostsResponseDto dto = postsService.findById(id);
-//        model.addAttribute("post", dto);
-//
-//        return "posts-update";
-//    }
 }
