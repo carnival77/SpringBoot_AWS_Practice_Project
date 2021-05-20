c18da855af51d49d7bc640f32bae178758e45978
carnival77 <stam0325@hanmail.net>
2021-05-20 16:57:50 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -43,11 +43,9 @@ echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name : $JAR_NAME | application-oauth.properties, application-real-db.properties 적용 "
 
-nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
-
-nohup java -jar \
-	-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \ $REPOSITORY/$JAR_NAME 2>&1 &
-
-nohup java -jar \ -Dspring.config.location=classpath:/application.properties, /home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \ -Dspring.profiles.active=real \ $REPOSITORY/$JAR_NAME 2>$1 $
+nohup java -jar \ 
+	-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \ 
+	-Dspring.profiles.active=real \ 
+	$REPOSITORY/$JAR_NAME 2>&1 &
